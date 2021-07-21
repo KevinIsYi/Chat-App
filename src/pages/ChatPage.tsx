4f62a15019516195c26249f7c9dcee322f64609c
KevinIsYi <rodriguezkevin95g@gmail.com>
2021-07-21 11:02:52 -0500

    Finished with sidebar

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -5,7 +5,7 @@ export const ChatPage = () => {
         <div className="h-screen mx-auto md:px-20">
             <div className="flex space-between h-screen bg-white shadow-lg">
                 <Sidebar />
-                <div className="w-8/12 bg-black">
+                <div className="w-8/12 shadow">
                     Chat
                 </div>
             </div>
