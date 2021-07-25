1d778008b246fcf7ee98060f1a1d958a699b53b6
Kevin Rodríguez <57973604+KevinIsYi@users.noreply.github.com>
2021-07-25 13:36:00 -0500

    Deleted dist dolfer

--- chat-app-backend/dist/helpers/removeSpaces.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.removeSpaces = void 0;
-const removeSpaces = (str) => str.trim().replace(/ +/g, ' ').toLowerCase();
-exports.removeSpaces = removeSpaces;
-//# sourceMappingURL=removeSpaces.js.map
